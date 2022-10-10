f228ce5e924945529478cb74e545f47fdb466bd0
JanMakel <114388767+JanMakel@users.noreply.github.com>
2022-10-10 10:41:15 +0200

    Cosa de scripts

---
+++ Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    /*
+     This is a multiline comment:
+     4 variables with the player information
+     */
+    public int playerAge = 33;//This varible saves the player's age
+    public float playerSpeed = 3.125f; //Ths variable saves the player's speed
+    public string playerName = "Jan"; // This variable saves the player's name
+    public bool gameOver = false;//This variable saves if player's have lost or not
+}
